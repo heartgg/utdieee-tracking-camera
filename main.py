14a2e5f91aafc877045768f4290ef93334f42ae9
Evan Soloway <52336842+evan-soloway@users.noreply.github.com>
2023-04-19 16:31:25 -0500

    added stream class. shut down with 'q' no longer works

--- main.py
+++ main.py
@@ -1,76 +1,118 @@
+from __future__ import print_function
+
+#import videoStreamClass.py
+from imutils.video import webcamvideostream
+import datetime
 import cv2
 import time
 from threading import Thread
 
-# defines a webcam object
-# index specifies camera to operate. 0 in this case uses my webcam
-cap = cv2.VideoCapture(0)  # self.stream = cv2.VideoCapture(src)
+class WebcamVideoStream:
+    def __init__(self, src=0):
+        self.stream = cv2.VideoCapture(src)
+        (self.success, self.img) = self.stream.read()
+        self.stopped = False
 
-# Initialize tracker \
-tracker = cv2.legacy.TrackerCSRT_create()
-tracker = cv2.TrackerCSRT_create()
+    def start(self):
 
-success, img = cap.read()  # (self.grabbed, self.frame) = self.stream.read()
-boundingBox = cv2.selectROI("Tracking", img, True, False)
+        Thread(target=self.update, args=()).start()
+        return self
 
-# Initialize tracker with bounding box
-tracker.init(img, boundingBox)
-##### TRACKER STUFF FINISHED /
+    def update(self):
 
-# verify camera opened
-if not cap.isOpened():  # isOpened() returns a T/F value depending on success of camera opening
-    print("Cannot open camera")
-    exit()
+        while True:
 
-prevFrameTime = 0
-newFrameTime = 0
+            if self.stopped:
+                return
 
+            (self.success, self.img) = self.stream.read()
 
-def drawBox(img, boundingBox):
-    x, y, w, h = int(boundingBox[0]), int(boundingBox[1]), int(boundingBox[2]), int(boundingBox[3])
-    cv2.rectangle(img, (x, y), ((x + w), (y + h)), (255, 0, 255), 3, 1)
+            #return self.success
 
-    # Calculate center coordinates
+    def read(self):
+        return self.img
 
-    centerX = int(x + (w / 2))
-    centerY = int(y + (h / 2))
-    cv2.circle(img, (centerX, centerY), 5, (255, 0, 255), -1)
-    print("X:", str(centerX), "Y:", str(centerY))
+    def stop(self):
+        self.stopped = True
 
-    cv2.putText(img, "Tracking", (75, 75), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0, 255, 0), 2)
+def main():
 
+    # from imutils import FPS
+    # from imutils import WebcamVideoStream
 
-def readLoop(prevFrameTime, newFrameTime):
-    #### MAKE FUNCTION (SHOULD BE THREAD FOR READING)
-    while True:
+    cap = WebcamVideoStream()
+    cap.start()
+    img = cap.read()
+    # defines a webcam object
+    # index specifies camera to operate. 0 in this case uses my webcam
 
-        readSuccess, img = cap.read()
-        boxSuccess, boundingBox = tracker.update(img)  # Updates the bounding box
-        # print(boundingBox)
+    # Initialize tracker \
+    tracker = cv2.legacy.TrackerCSRT_create()
+    tracker = cv2.TrackerCSRT_create()
 
-        if boxSuccess:
-            drawBox(img, boundingBox)
-        else:
-            cv2.putText(img, "Lost", (75, 75), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0, 0, 255), 2)
+    # success, img = cap.read()  # (self.grabbed, self.frame) = self.stream.read()
+    boundingBox = cv2.selectROI("Tracking", img, True, False)
 
-        # Calculates FPS
-        newFrameTime = time.time()
-        fps = 1 / (newFrameTime - prevFrameTime)
-        prevFrameTime = newFrameTime
+    # Initialize tracker with bounding box
+    tracker.init(img, boundingBox)
+    ##### TRACKER STUFF FINISHED /
 
-        # displays formatting on video feed
-        cv2.putText(img, str(int(fps)), (75, 50), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0, 0, 255), 2)
-        cv2.imshow("Tracking", img)
+    # verify camera opened
+    """if not cap.update.isOpened():  # isOpened() returns a T/F value depending on success of camera opening
+        print("Cannot open camera")
+        exit()"""
 
-        # If q key has been pressed, stop program
-        if cv2.waitKey(1) & 0xff == ord('q'):
-            break
-    ##### MAKE FUNCTION
+    prevFrameTime = 0
+    newFrameTime = 0
 
 
-Thread(target=readLoop(prevFrameTime, newFrameTime), args=()).start()
+    def drawBox(img, boundingBox):
+        x, y, w, h = int(boundingBox[0]), int(boundingBox[1]), int(boundingBox[2]), int(boundingBox[3])
+        cv2.rectangle(img, (x, y), ((x + w), (y + h)), (255, 0, 255), 3, 1)
 
-readLoop(prevFrameTime, newFrameTime)
+        # Calculate center coordinates
 
-# release the capture
-cap.release()
+        centerX = int(x + (w / 2))
+        centerY = int(y + (h / 2))
+        cv2.circle(img, (centerX, centerY), 5, (255, 0, 255), -1)
+        print("X:", str(centerX), "Y:", str(centerY))
+
+        cv2.putText(img, "Tracking", (75, 75), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0, 255, 0), 2)
+
+
+    def readLoop(prevFrameTime, newFrameTime):
+        #### MAKE FUNCTION (SHOULD BE THREAD FOR READING)
+        while True:
+
+            img = cap.read()
+            boxSuccess, boundingBox = tracker.update(img)  # Updates the bounding box
+            # print(boundingBox)
+
+            if boxSuccess:
+                drawBox(img, boundingBox)
+            else:
+                cv2.putText(img, "Lost", (75, 75), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0, 0, 255), 2)
+
+            # Calculates FPS
+            newFrameTime = time.time()
+            fps = 1 / (newFrameTime - prevFrameTime)
+            prevFrameTime = newFrameTime
+
+            # displays formatting on video feed
+            cv2.putText(img, str(int(fps)), (75, 50), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0, 0, 255), 2)
+            cv2.imshow("Tracking", img)
+
+            # If q key has been pressed, stop program
+            if cv2.waitKey(1) & 0xff == ord('q'):
+                break
+        ##### MAKE FUNCTION
+
+
+    Thread(target=readLoop(prevFrameTime, newFrameTime), args=()).start()
+
+    readLoop(prevFrameTime, newFrameTime)
+
+    # release the capture
+    cap.release()
+
+main()
