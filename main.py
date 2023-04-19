0f12be0834ad5bee13cb4a1ce6c93ed1e8b2ee75
Evan Soloway <52336842+evan-soloway@users.noreply.github.com>
2023-04-19 12:09:13 -0500

    added threading. no noticeable improvement. make sure current and next frames are processed on different threads.

--- main.py
+++ main.py
@@ -1,76 +1,76 @@
-
 import cv2
 import time
+from threading import Thread
 
 # defines a webcam object
 # index specifies camera to operate. 0 in this case uses my webcam
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)  # self.stream = cv2.VideoCapture(src)
 
-
-# Initialize tracker
+# Initialize tracker \
 tracker = cv2.legacy.TrackerCSRT_create()
+tracker = cv2.TrackerCSRT_create()
 
-success, img = cap.read()
-
+success, img = cap.read()  # (self.grabbed, self.frame) = self.stream.read()
 boundingBox = cv2.selectROI("Tracking", img, True, False)
 
 # Initialize tracker with bounding box
-tracker.init(img,boundingBox)
-
-
+tracker.init(img, boundingBox)
+##### TRACKER STUFF FINISHED /
 
 # verify camera opened
-if not cap.isOpened(): # isOpened() returns a T/F value depending on success of camera opening
+if not cap.isOpened():  # isOpened() returns a T/F value depending on success of camera opening
     print("Cannot open camera")
     exit()
-
 
 prevFrameTime = 0
 newFrameTime = 0
 
+
 def drawBox(img, boundingBox):
     x, y, w, h = int(boundingBox[0]), int(boundingBox[1]), int(boundingBox[2]), int(boundingBox[3])
-    cv2.rectangle(img, (x,y), ((x+w), (y+h)), (255, 0, 255), 3, 1)
+    cv2.rectangle(img, (x, y), ((x + w), (y + h)), (255, 0, 255), 3, 1)
 
-    #Calculate center coordinates
-    centerX = int(x + (w/2))
-    centerY = int(y + (h/2))
+    # Calculate center coordinates
+
+    centerX = int(x + (w / 2))
+    centerY = int(y + (h / 2))
     cv2.circle(img, (centerX, centerY), 5, (255, 0, 255), -1)
-    print( "X:", str(centerX), "Y:", str(centerY))
+    print("X:", str(centerX), "Y:", str(centerY))
 
     cv2.putText(img, "Tracking", (75, 75), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0, 255, 0), 2)
 
-while True:
 
-    readSuccess, img = cap.read()
+def readLoop(prevFrameTime, newFrameTime):
+    #### MAKE FUNCTION (SHOULD BE THREAD FOR READING)
+    while True:
 
-    boxSuccess, boundingBox = tracker.update(img) # Updates the bounding box
-    #print(boundingBox)
+        readSuccess, img = cap.read()
+        boxSuccess, boundingBox = tracker.update(img)  # Updates the bounding box
+        # print(boundingBox)
+
+        if boxSuccess:
+            drawBox(img, boundingBox)
+        else:
+            cv2.putText(img, "Lost", (75, 75), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0, 0, 255), 2)
+
+        # Calculates FPS
+        newFrameTime = time.time()
+        fps = 1 / (newFrameTime - prevFrameTime)
+        prevFrameTime = newFrameTime
+
+        # displays formatting on video feed
+        cv2.putText(img, str(int(fps)), (75, 50), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0, 0, 255), 2)
+        cv2.imshow("Tracking", img)
+
+        # If q key has been pressed, stop program
+        if cv2.waitKey(1) & 0xff == ord('q'):
+            break
+    ##### MAKE FUNCTION
 
 
-    if boxSuccess:
-        drawBox(img, boundingBox)
-    else:
-        cv2.putText(img, "Lost", (75, 75), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0, 0, 255), 2)
+Thread(target=readLoop(prevFrameTime, newFrameTime), args=()).start()
 
-
-    # Calculates FPS
-    newFrameTime = time.time()
-    fps = 1/ (newFrameTime - prevFrameTime)
-    prevFrameTime = newFrameTime
-
-
-
-    # displays formatting on video feed
-    cv2.putText(img, str(int(fps)), (75, 50), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0, 0, 255), 2)
-    cv2.imshow("Tracking", img)
-
-    # If q key has been pressed, stop program
-    if cv2.waitKey(1) & 0xff == ord('q'):
-        break
-
+readLoop(prevFrameTime, newFrameTime)
 
 # release the capture
 cap.release()
-
-
